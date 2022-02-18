592894fed8a395043c592fcbb1546019471ff62f
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-18 16:54:02 -0600

    added useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import { useState, useEffect }  from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
   const [count, setCount] = useState(0);
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => setApiData(json))
+      
+      
+  })
   return (
     <div className="App">
-      
+
       <h3>{count}</h3>
       <button onClick={() => setCount(count + 1)}>click me to count up</button>
       <button onClick={() => setCount(count - 1)}>click me to count down</button>
+
+      <div>{apiData.map(data => (
+        <div>
+          <h1>{data.name}</h1>
+          <p>{data.email}</p>
+        
+          
+        </div>
+
+      ))}</div>
     </div>
   );
 }
