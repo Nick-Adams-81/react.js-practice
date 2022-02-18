700e6e74e569641200f55ba7623e6da6abe65498
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-18 16:38:21 -0600

    added first useState hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { useState, useEffect }  from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}>click me to count up</button>
+      <button onClick={() => setCount(count - 1)}>click me to count down</button>
     </div>
   );
 }
