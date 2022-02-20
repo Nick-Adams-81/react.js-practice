ac9f6aed9c108745d8f02e46e3a6bd7ad5c8c9ac
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-20 10:30:21 -0600

    added useRef hook

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import { useState, useEffect, useContext, createContext } from 'react';
+import {
+  useState,
+  useEffect,
+  useContext,
+  createContext,
+  useRef
+} from 'react';
 import './App.css';
 
 function App() {
 
-  const [count, setCount] = useState(0);
-  const [apiData, setApiData] = useState([]);
+  
+  const [count, setCount] = useState(0); // setting initial state of count to 0
+  const [apiData, setApiData] = useState([]); // setting initial state of apiData to an empty array so we can map the data in the JSX
 
+  // useRef hook 
+  const myBtn = useRef(null)
+  const clickIt = () => myBtn.current.click()
+
+  // useEffect takes the place of the 3 life cycle methods in class components(componentDidMount, componentDidUpdate, componentWillUnmount)
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(json => setApiData(json))
-      return () => alert("component is gone!")
-         
+    return () => alert("component is gone!")
+
   }, [])
 
-  const moods = { 
+  // creating a variable for testing with useContext
+  const moods = {
     happy: "happy",
     sad: "sad"
   }
 
+    // using createContext to use inside useContext
+  const MoodContext = createContext(moods)
+
+  // creating a method to return JSX with current mood
   const MoodEmoji = () => {
     const mood = useContext(MoodContext)
     return <p>{mood}</p>
   }
 
-  const MoodContext = createContext(moods)
+
   return (
     <div className="App">
-      <MoodContext.Provider value={moods.sad}>
+      <MoodContext.Provider value={moods.happy}>
         <MoodEmoji />
       </MoodContext.Provider>
 
@@ -39,11 +56,13 @@ function App() {
         <div>
           <h1>{data.name}</h1>
           <p>{data.email}</p>
-        
-          
+
+
         </div>
 
       ))}</div>
+
+      <button ref={myBtn}>click me</button>
     </div>
   );
 }
